2139b191e45ba870459aeb375fb933947b105d2b
Version Update <sorin.marica@jeco.dev>
2021-07-05 11:45:37 +0000

    Automatic Version Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='webscrapingapi',
-    version="1.2.2",
+    version="1.2.3",
     url='https://github.com/WebScrapingAPI/webscrapingapi-python',
     description='WebScrapingApi Python SDK',
     long_description=open('README.md').read(),
